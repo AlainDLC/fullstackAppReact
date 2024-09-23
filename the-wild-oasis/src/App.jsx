4db6c42a62fb93ab5177e6dabbf13becc8b1d098
AlainDLC <jd_alain@hotmail.com>
2024-09-23 16:42:46 +0200

    exploring styled component

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,5 +1,28 @@
-function App() {
-  return <h1>nu börja vi</h1>;
-}
+import styled from "styled-components";
+import GlobaStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 2rem;
+`;
+
+const App = () => {
+  return (
+    <>
+      <GlobaStyles />
+      <StyledApp>
+        <Heading type="h1">The Wild Oasis</Heading>
+        <Heading as="h2">Check In and Out</Heading>
+        <Button onClick={() => alert("test")}>Check in</Button>
+        <Button onClick={() => alert("test")}>Check out</Button>
+        <Input type="number" placeholder="Number of guests" />
+        <Heading as="h3">Form</Heading>
+      </StyledApp>
+    </>
+  );
+};
 
 export default App;
